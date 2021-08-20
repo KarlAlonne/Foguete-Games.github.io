fded6065449c54fe266ab3a07f7d26917cf4fb8b
Karl Lenin <karlalencar14@outlook.com>
2021-08-20 16:49:28 -0300

    Adicionando ao arquivo jogo.js, os comandos referentes ao sprite bat1.png

--- js/jogo.js
+++ js/jogo.js
@@ -5,3 +5,24 @@ sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.get('2d');
+
+const BatAtack = {
+    spriteX:18,
+    spriteY:29,
+    largura: 75,
+    altura: 67,    
+    x: 10,
+    y: 50,
+}
+
+function loop() {
+    contexto.drawImage(
+        sprites,
+        BatAtack.spriteX, BatAtack.spriteY,
+        BatAtack.largura, BatAtack.altura,
+        BatAtack.x, BatAtack.y,
+        BatAtack.largura, BatAtack.altura,
+         
+        );
+    requestAnimationFrame(loop);
+}
