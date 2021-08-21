cbcce515d916dc708a9c991c974b3cca5a97ed7b
Karl Lenin <karlalencar14@outlook.com>
2021-08-20 22:50:26 -0300

    Ajustes de certos comandos ao arquivo Jogo.js é acrescentamento da variável chao ao arquivo.

--- js/jogo.js
+++ js/jogo.js
@@ -6,6 +6,34 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.get('2d');
 
+const chao = {
+    spriteX:0,
+    spriteY:0,
+    largura:401,
+    altura:112,
+    x:0,
+    y: canvas.height - 112,
+    desenha() {
+        contexto.drawImage (
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+            
+        );
+
+        contexto.drawImage (
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura,
+        );
+    }
+}
+
+
 const BatAtack = {
     spriteX:18,
     spriteY:29,
@@ -13,16 +41,21 @@ const BatAtack = {
     altura: 67,    
     x: 10,
     y: 50,
-}
-
-function loop() {
-    contexto.drawImage(
+   desenha () {
+     contexto.drawImage(
         sprites,
         BatAtack.spriteX, BatAtack.spriteY,
         BatAtack.largura, BatAtack.altura,
         BatAtack.x, BatAtack.y,
         BatAtack.largura, BatAtack.altura,
-         
         );
+     }
+    }
+
+function loop() {
+    BatAtack.desenha();
+    chao.desenha();
+
+    
     requestAnimationFrame(loop);
 }
